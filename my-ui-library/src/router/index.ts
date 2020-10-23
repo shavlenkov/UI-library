eb9a6a05805f4daa04ce87fd2f7a09bfda54cf60
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-23 20:08:57 +0300

    router

--- my-ui-library/src/router/index.ts
+++ my-ui-library/src/router/index.ts
@@ -1,7 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
+import MyButtonView from '../views/MyButtonView.vue'
+
 const routes: Array<RouteRecordRaw> = [
 
+  {
+    path: '/MyButton',
+    name: 'MyButtonView',
+    component: MyButtonView
+  },
+  
 ]
 
 const router = createRouter({
