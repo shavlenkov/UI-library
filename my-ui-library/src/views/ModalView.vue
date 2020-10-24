0c8e9c6480aa0cc5df425b44b6630fb65c2f8e45
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-24 11:27:05 +0300

    ModalView

---
+++ my-ui-library/src/views/ModalView.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <Modal button="Show">
+
+    <template v-slot:header>
+      <h1>Header</h1>
+    </template>
+
+    <template v-slot:body>
+      <h3>Body</h3>
+    </template>
+
+    <template v-slot:footer>
+      <h4>Footer</h4>
+    </template>
+    
+  </Modal>
+  
+</template>
+
+<script lang="ts">
+
+  import { defineComponent } from 'vue';
+
+  import Modal from '@/components/Modal.vue';
+  
+  export default defineComponent({
+
+    name: "ModalView",
+
+    components: {
+      Modal,
+    },
+
+  });
+
+</script>
