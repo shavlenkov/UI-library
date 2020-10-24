88b4480d786b264c2d1d9a9598d718e1a44012a0
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-24 12:04:25 +0300

    Carousel

---
+++ my-ui-library/src/components/Carousel.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="carousel">
+    <div class="carousel-item">
+      <img v-bind:src="slides[Math.abs(index) % slides.length]"/>
+    </div>
+
+    <p v-on:click="index--"><a class="prev">&lt;</a></p>
+    <p v-on:click="index++"><a class="next">&gt;</a></p>
+  
+  </div>
+</template>
+
+<script lang="ts">
+
+  import { defineComponent } from 'vue';
+   
+  export default defineComponent({
+    
+    name: "Carousel",
+
+    props: ["slides"],
+
+    data() {
+
+      return {
+
+        index: 0,
+
+      }
+
+    },
+    
+  });
+
+</script>
+
+<style lang="less">
+
+@black: #000;
+
+.carousel {
+  max-width: 50%;
+  height: 400px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.carousel-item img {
+  width: 50%;
+  height: 50%;
+  margin-top: 40px;
+}
+
+.prev, .next {
+  background: @black;
+  color: white;
+  font-size: 18px;
+  position: absolute;
+  top: 50%;
+  padding: 16px;
+  cursor: pointer;
+  margin-left: -30%;
+  margin-right: 20%;
+}
+
+.next {
+  right: 0;
+}
+
+</style>
