4b81ef219d7496cdf9009e34e10795655ed329eb
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-24 12:04:48 +0300

    CarouselView

---
+++ my-ui-library/src/views/CarouselView.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <Carousel :slides="slides"/>
+
+</template>
+
+<script lang="ts">
+
+  import { defineComponent } from 'vue';
+
+  import Carousel from '@/components/Carousel.vue';
+
+  export default defineComponent({
+
+    name: "CarouselView",
+
+    components: {
+      Carousel,
+    },
+
+    data() {
+
+      return {
+
+        slides: ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png", "https://pluralsight.imgix.net/paths/path-icons/angular-14a0f6532f.png"],
+
+      }
+
+    }  
+        
+  });
+
+</script>
