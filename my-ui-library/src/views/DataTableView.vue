9d988e3f116865574c2a31cbde81841ba36c1d22
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-24 17:51:19 +0300

    DataTableView

---
+++ my-ui-library/src/views/DataTableView.vue
@@ -0,0 +1,44 @@
+<template>
+  
+  <DataTable :items="config" :columns="users" :search="search"/>
+
+</template>
+
+<script lang="ts">
+
+  import { defineComponent } from 'vue';
+  
+  import DataTable from '@/components/DataTable.vue';
+  
+  export default defineComponent({
+
+    name: "DataTableView",
+
+    components: {
+      DataTable,
+    },
+    
+    data() {
+
+      return {
+
+        config: [
+          {title: '№', value: 'id', sortable: true},
+          {title: 'Имя', value: 'name'},
+          {title: 'Фамилия', value: 'surname'},
+          {title: 'Возраст', value: 'age'},
+        ],
+
+        users: [
+          {id: 1, name: 'Вася', surname: 'Петров', age: 20},
+          {id: 2, name: 'Петя', surname: 'Васечкин', age: 16},
+        ],
+
+        search: {
+          fields: [],
+        },
+      }
+    }
+  });
+
+</script>
