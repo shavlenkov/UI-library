f1eb9b17b5a91ddb619eb5987ab9e2392bc763a2
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-26 15:15:47 +0200

    MyButtonView

--- my-ui-library/src/views/MyButtonView.vue
+++ my-ui-library/src/views/MyButtonView.vue
@@ -1,5 +1,7 @@
 <template>
 
+  <h1>MyButton</h1>
+
   <MyButton color="error">Button</MyButton>
   <MyButton color="success">Button</MyButton>
   <MyButton color="warning">Button</MyButton>
@@ -8,6 +10,10 @@
   <MyButton color="white">Button</MyButton>
   <MyButton color="primary">Button</MyButton>
   <MyButton color="secondary">Button</MyButton>
+
+  <br/><br/>
+
+  <img src="../../public/MyButtonCode/code1.png"/>
 
   <br/><br/>
 
@@ -22,9 +28,17 @@
 
   <br/><br/>
 
+  <img src="../../public/MyButtonCode/code2.png"/>
+
+  <br/><br/>
+
   <MyButton color="primary" size="smoll">Button</MyButton>
   <MyButton color="primary" size="standart">Button</MyButton>
   <MyButton color="primary" size="big">Button</MyButton>
+
+  <br/><br/>
+
+  <img src="../../public/MyButtonCode/code3.png"/>
 
   <br/><br/>
 
@@ -33,12 +47,34 @@
 
   <br/><br/>
 
+  <img src="../../public/MyButtonCode/code4.png"/>
+
+  <br/><br/>
+
   <MyButton color="primary" size="standart" shape="edge" state="outlined">Button</MyButton>
   <MyButton color="primary" size="standart" shape="edge" state="disabled" disabled>Button</MyButton>
 
   <br/><br/>
 
+  <img src="../../public/MyButtonCode/code5.png"/>
+
+  <br/><br/>
+
   <MyButton color="primary" size="standart" shape="edge" :click="message">Button</MyButton>
+
+  <br/><br/>
+
+  <img src="../../public/MyButtonCode/code6.png"/>
+  <img src="../../public/MyButtonCode/code7.png"/>
+
+  <br/><br/>
+
+  <DataTable :items="thead" :columns="tbody" :search="search"/>
+
+  <vue-markdown> 
+
+   ##Test
+  </vue-markdown>
 
 </template>
 
@@ -47,6 +83,8 @@
   import { defineComponent } from 'vue';
 
   import MyButton from '@/components/MyButton.vue';
+  import DataTable from '@/components/DataTable.vue';
+
   
   export default defineComponent({
 
@@ -54,6 +92,7 @@
 
     components: {
       MyButton,
+      DataTable,
     },
 
     methods: {
@@ -61,6 +100,34 @@
         alert("Hello, world!");
       }
     },
+
+    data() {
+
+      return {
+
+        thead: [
+          {title: 'Название', value: 'name', sortable: true},
+          {title: 'Тип', value: 'type'},
+          {title: 'Дефолтное значение', value: 'default'},
+          {title: 'Обязательность', value: 'obligation'},
+          {title: 'Описание', value: 'description'},
+        ],
+
+        tbody: [
+          {name: 'color', type: 'String', default: 'secondary', obligation: 'Необязательный', description: 'Этот проп устанавливает цвет кнопки'},
+          {name: 'size', type: 'String', default: 'standart', obligation: 'Необязательный', description: 'Этот проп устанавливает размер кнопки'},
+          {name: 'shape', type: 'String', default: 'edge', obligation: 'Необязательный', description: 'Этот проп устанавливает форму кнопки'},
+          {name: 'state', type: 'String', default: '-', obligation: 'Необязательный', description: 'Этот проп устанавливает состояние кнопки'},
+          {name: ':click', type: 'Object', default: '-', obligation: 'Необязательный', description: 'Этот проп устанавливает событие клик на кнопку'},
+        ],
+
+        search: {
+          fields: [],
+        },
+
+      }
+
+    }
 
   });
 
