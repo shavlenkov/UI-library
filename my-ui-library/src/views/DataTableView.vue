7ed48290c4f00e977f08900e20af1fcbd9b7e2c4
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-26 15:14:30 +0200

    DataTableView

--- my-ui-library/src/views/DataTableView.vue
+++ my-ui-library/src/views/DataTableView.vue
@@ -1,6 +1,17 @@
 <template>
+
+  <h1>DataTable</h1>
   
   <DataTable :items="config" :columns="users" :search="search"/>
+
+  <br/><br/>
+
+  <img src="../../public/DataTableCode/code1.png"/>
+  <img src="../../public/DataTableCode/code2.png"/>
+
+  <br/><br/>
+
+  <DataTable :items="thead" :columns="tbody" :search="search"/>
 
 </template>
 
@@ -34,11 +45,34 @@
           {id: 2, name: 'Петя', surname: 'Васечкин', age: 16},
         ],
 
+        thead: [
+          {title: 'Название', value: 'name', sortable: true},
+          {title: 'Тип', value: 'type'},
+          {title: 'Дефолтное значение', value: 'default'},
+          {title: 'Обязательность', value: 'obligation'},
+          {title: 'Описание', value: 'description'},
+        ],
+
+        tbody: [
+          {name: ':items', type: 'Array', default: '-', obligation: 'Обязательный', description: 'Этот проп устанавливает шапку таблицы'},
+          {name: ':columns', type: 'Array', default: '-', obligation: 'Обязательный', description: 'Этот проп устанавливает тело таблицы'},
+          {name: ':search', type: 'Object', default: '-', obligation: 'Необязательный', description: 'Этот проп устанавливает тело таблицы'},
+        ],
+
         search: {
           fields: [],
         },
+
       }
+
     }
+    
   });
 
 </script>
+
+
+
+
+
+
