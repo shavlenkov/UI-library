d6f1e343616ddac2b7ac05edf54e59a94bddd738
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2020-10-30 22:55:34 +0200

    app

--- my-ui-library/src/App.vue
+++ my-ui-library/src/App.vue
@@ -1,29 +1,31 @@
 <template>
+
 <Navbar>
 
     <template #brand>
 
-      <img class="logo" src="../public/logo.png" width="280" height="50"/>
+      <img class="logo" src="../public/logo.png" width="270" height="50"/>
 
     </template>
 
     <template #menu>
+
+      <router-link class="router-link" to="/Home">Home</router-link>
       
-      <router-link to="/MyButton">MyButton</router-link>
+      <router-link class="router-link" to="/MyButton">MyButton</router-link>
 
-      <router-link to="/Grid">Grid</router-link>
+      <router-link class="router-link" to="/Grid">Grid</router-link>
 
-      <router-link to="/Modal">Modal</router-link>
+      <router-link class="router-link" to="/Modal">Modal</router-link>
 
-      <router-link to="/Carousel">Carousel</router-link>
+      <router-link class="router-link" to="/Carousel">Carousel</router-link>
 
-      <router-link to="/DataTable">DataTable</router-link>
+      <router-link class="router-link" to="/DataTable">DataTable</router-link>
 
     </template>
 
   </Navbar>
 
-  <br/><br/>
 
   <div class="view">
 
@@ -31,55 +33,58 @@
 
   </div>
 
-  <div class="count-particles">
-    <span class="js-count-particles">--</span>
-  </div>
-
-  
-
 </template>
 
 <style lang="less">
 
   body {
     background: url("../public/starry_sky.jpg");
+    background-repeat: repeat;
   }
 
   .view {
-
-    background: linear-gradient(180deg, #ff7700, #fff200, #22ff00, #4059ff);
-    background-size: 400% 400%;
-    animation: gradient 15s ease infinite;
-
-    @keyframes gradient {
-
-      0% {
-        background-position: 0% 25%;
-      }
-
-      25% {
-        background-position: 25% 50%;
-      }
-
-      50% {
-        background-position: 50% 75%;
-      }
-
-      75% {
-        background-position: 75% 100%;
-      }
-
-      100% {
-        background-position: 100% 0%;
-      } 
-
-    }
-
-    margin-top: -60px;
+    background: linear-gradient(to bottom, #00ffc8, #006eff);
   }
 
-  h1 {
+  @media (max-width: 767px) {
+
+    .view {
+      width: 100%;
+      margin: 0 auto;
+      margin-top: -20px;
+    }
+
+  }
+
+  @media (min-width: 768px) and (max-width: 991px) {
+
+    .view {
+      width: 600px;
+      margin: 0 auto;
+      margin-top: -20px;
+    }
+
+  }
+
+  @media (min-width: 992px) {
+
+    .view {
+      width: 900px;
+      margin: 0 auto;
+      margin-top: -20px;
+    }
+
+  }
+
+  h1, h3 {
     font-family: 'Arial';
+  }
+
+  code {
+    display: inline-block;
+    background: #303030;
+    color: white;
+    font-size: 15px;
   }
 
 </style>
@@ -99,6 +104,5 @@
     },
 
   });
-
-  
+ 
 </script>
